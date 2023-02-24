c8534d1c7b17501bd16ec08a56be81d22868655a
Amit Gaikwad <amitmanojgaikwad@gmail.com>
2023-02-24 08:21:41 +0530

    basic structure

---
+++ src/requests.js
@@ -0,0 +1,14 @@
+const APIKEY = "6969573ae0c3461697c7c890b7e9ec28";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumantaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+export default requests;
